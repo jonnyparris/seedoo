f2e1ec1f107fcf1edafee10a5fec28ee0029cf67
jonnyparris <6400000+jonnyparris@users.noreply.github.com>
2023-07-25 15:36:31 +0100

    init nuxt3, fetch tasks from todoist

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { TodoistApi } from '@doist/todoist-api-typescript'
+
+const runtimeConfig = useRuntimeConfig()
+
+const api = new TodoistApi(runtimeConfig.todoistApiKey)
+
+const { data: tasks } = await useAsyncData('todoist', () => {
+  return api.getTasks()
+})
+
+</script>
+
+<template>
+  <div>
+    <h2>Active Tasks</h2>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.content }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
